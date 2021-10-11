a50c7c2b0e71700192950097543c98b8cd398d49
Douglas-Marques <douglasmarques09@gmail.com>
2021-10-11 05:11:13 -0300

    create posts page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
-import { GetStaticProps } from 'next';
+import Head from 'next/head'
+import { GetStaticProps } from 'next'
+import Link from 'next/link'
+import Prismic from '@prismicio/client'
+import { FiCalendar, FiUser } from 'react-icons/fi'
+import { format } from 'date-fns'
 
-import { getPrismicClient } from '../services/prismic';
+import { getPrismicClient } from '../services/prismic'
 
-import commonStyles from '../styles/common.module.scss';
-import styles from './home.module.scss';
+import commonStyles from '../styles/common.module.scss'
+import styles from './home.module.scss'
 
 interface Post {
   uid?: string;
@@ -24,13 +29,64 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Posts | spacetraveling</title>
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+      <main className={commonStyles.container}>
+        <div className={styles.posts}>
+          {postsPagination.results.map(post => (
+            <Link href={`/post/${post.uid}`} key={post.uid}>
+              <a>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <div>
+                  <time><FiCalendar size={22} /> {post.first_publication_date}</time>
+                  <span><FiUser size={22} /> {post.data.author}</span>
+                </div>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </main>
 
-//   // TODO
-// };
+      {postsPagination.next_page && (
+        <a className={styles.more}> Carregar mais posts</a>
+      )}
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+  const postsResponse = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts')
+  ], {
+    fetch: ['posts.title', 'posts.subtitle', 'posts.author']
+  })
+
+  const results: Post[] = postsResponse.results.map(post => ({
+    uid: post.uid,
+    first_publication_date: format(new Date(post.first_publication_date), 'dd MMM yyyy'),
+    data: {
+      title: post.data.title ?? '',
+      subtitle: post.data.subtitle ?? '',
+      author: post.data.author ?? '',
+    }
+  }))
+
+  const postsPagination: PostPagination = {
+    next_page: postsResponse.next_page,
+    results
+  }
+
+  return {
+    props: {
+      postsPagination
+    },
+    revalidate: 60 * 30 //30min
+  }
+}
